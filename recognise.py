2f3209aaf36de749570377b914c6d05b091a9cf6
shashank <shashankkrishnanaik@gmail.com>
2022-05-08 17:05:32 +0530

    add sklearn dependency5

--- recognise.py
+++ recognise.py
@@ -1,10 +1,19 @@
 import sys
 import joblib
+# from sklearn import neighbors
+# import pandas as pd
+
+# dataset = pd.read_csv('newData.csv')
+# x = dataset[['0','2', '3', '4', '6', '7', '8', '10', '11', '12', '14', '15', '16', '18', '19', '20']].values
+# y = dataset['letter'].values
+
+# model=neighbors.KNeighborsClassifier().fit(x,y)
 
 handTest = sys.argv[1].split(',')
 handTest = list(map(int,handTest))
 
-model = joblib.load('hand_model.pkl')
+model = joblib.load('hand_model_new_nv.pkl')
+
 
 output=model.predict([handTest])
 
