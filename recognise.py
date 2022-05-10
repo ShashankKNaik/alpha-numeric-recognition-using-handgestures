e1acfbe809378f700ffa6eeceec47dc87b4faeed
shashank <shashankkrishnanaik@gmail.com>
2022-05-10 12:01:36 +0530

    fix. increase model acurecy

--- recognise.py
+++ recognise.py
@@ -4,7 +4,7 @@ import joblib
 handTest = sys.argv[1].split(',')
 handTest = list(map(int,handTest))
 
-model = joblib.load('hand_model_new_nv.pkl')
+model = joblib.load('hand_model_k5_2.pkl')
 
 output=model.predict([handTest])
 
